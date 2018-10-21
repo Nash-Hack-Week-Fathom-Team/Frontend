0d86e7c1c441383bbadce7ca6711b236856f3a1c
Elvis Hernandez <elvishernandez5@yahoo.com>
2018-10-21 13:21:11 -0500

    routing done

--- client/src/components/Main/Modules/Modules.js
+++ client/src/components/Main/Modules/Modules.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import Module from './Module/Module';
+import {Link} from 'react-router-dom';
 
 class Modules extends Component {
-    state = { 
+    state = {  
         questions: [
-            {id: 1, text: "Who am I?", link: "#"},
-            {id: 2, text: "What matters?", link: "#matters"},
-            {id: 3, text: "What's my purpose?", link: "#purpose"},
-            {id: 4, text: "What's my plan?", link: "#plan"}
+            {id: 1, text: "Who am I?", link: "/Focus"},
+            {id: 2, text: "What matters?", link: "/Focus"},
+            {id: 3, text: "What's my purpose?", link: "/Focus"},
+            {id: 4, text: "What's my plan?", link: "/Focus"}
         ]
      }
     render() { 
         return ( 
             <div class="questions">
                 {this.state.questions.map( question => (
-                    <Module key={question.id} text={question.text} link={question.text} selected/>
+                    <Module key={question.id} text={question.text} link={question.link} selected/>
                 ))}
+
             </div>
 
          );
