0ef5114a38c1c9dd9d3b140ccf7c7319b73feb16
Hellrungj <johnjameshellrung@gmail.com>
2018-10-21 12:43:47 -0500

    Save project for a pull

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json())
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
+const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
   ];
 
+// create a user
+//app.post('/api/users', (req, res) => {
+//  User.create(req.body)
+//      .then(user => res.json(user))
+//})
+// get all users
+//app.get('/api/users', (req, res) => {
+//  User.findAll().then(users => res.json(users))
+//})
+
+app.get('/api/customers', (req, res) => {
+  
   res.json(customers);
 });
 
